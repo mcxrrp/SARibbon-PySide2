2091200ff264d742fe9425da8e59919da003852c
mcxrrp <142974556+mcxrrp@users.noreply.github.com>
2023-09-03 05:26:13 +0200

    Add files via upload

---
+++ PySARibbon/SAWidgets/SARibbonControlButton.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Module     SARibbonControlButton
+@Author     ROOT
+"""
+from PySide2.QtWidgets import QToolButton
+
+
+class SARibbonControlButton(QToolButton):
+    def __init__(self, parent):
+        super().__init__(parent)
